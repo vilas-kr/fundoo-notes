bf939facb7a2c853c69c7a662770d819782cacd6
vilas <vilaskr2962@gmail.com>
2026-03-20 00:29:52 +0530

    Add Main: [ Vilas ] main file to run server

---
+++ src/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+from src.config.database import Base, engine
+from src.config.logging_middleware import log_requests
+from src.routes import user_routes
+
+app = FastAPI()
+
+# Create tables
+Base.metadata.create_all(bind=engine)
+
+app.middleware("http")(log_requests)
+
+app.include_router(user_routes.router)
